86429e04b29a0780ec2f4dc513355ef9a9b265fd
MRHS Robotics <mrhsrobotics@localhost.localdomain>
2019-11-29 14:19:58 -0700

    add initial 2019 TeleOp code to repo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BE2019_TeleOp.java
@@ -0,0 +1,100 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="BE2019_TeleOp", group="Iterative Opmode")
+public class BE2019_TeleOp extends OpMode
+{
+    private double power = 1;
+    private double direction = 1;
+    private double lowPower = 0.25;
+    private double highPower = 1;
+    private boolean xButtonHeld = false;
+    private boolean yButtonHeld = false;
+
+
+    RobotHardware robot = new RobotHardware();
+
+    public void init() {
+        telemetry.addData("Status", "BE2019 TeleOp Initialized");
+        robot.init(hardwareMap);
+
+    }
+
+    public void loop() {
+
+        double x = gamepad1.left_stick_x;
+        double y = gamepad1.left_stick_y;
+
+        ////          Driving         ////
+
+        // Slow Down
+        if (gamepad1.x) {
+            if (!xButtonHeld) {
+                if (power == highPower) {
+                    power = lowPower;
+                }
+                else {
+                    power = highPower;
+                }
+                xButtonHeld = true;
+            }
+        }
+        else {
+            xButtonHeld = false;
+        }
+
+        // Direction Change
+        if (gamepad1.y) {
+            if (!yButtonHeld) {
+                direction *= -1;
+            }
+            yButtonHeld = true;
+        }
+        else {
+            yButtonHeld = false;
+        }
+
+
+
+        // Driving Power
+        robot.leftFront.setPower((y + (x * direction)) * power);
+        robot.rightFront.setPower(((-y * direction) + x) * power);
+        robot.leftBack.setPower(((-y * direction) + x) * power);
+        robot.rightBack.setPower((y + (x * direction)) * power);
+
+        // Spinning
+        if (gamepad1.left_bumper){
+            robot.leftFront.setPower(-1 * power);
+            robot.rightFront.setPower(1* power);
+            robot.leftBack.setPower(-1 * power);
+            robot.rightBack.setPower(1 * power);
+        }
+        else if (gamepad1.right_bumper){
+            robot.leftFront.setPower(1 * power);
+            robot.rightFront.setPower(-1* power);
+            robot.leftBack.setPower(1 * power);
+            robot.rightBack.setPower(-1 * power);
+        }
+
+
+
+        // All For Telemetry //
+        double righty = gamepad1.right_stick_y;
+        double rightx = gamepad1.right_stick_x;
+        double lefty = gamepad1.left_stick_y;
+        double leftx = gamepad1.left_stick_x;
+
+        telemetry.addData("Status", "BE2019 TeleOp Running");
+        telemetry.addData("Joysticks", "Right Y:(%.2f), Right X:(%.2f), Left Y:(%.2f), Left X:(%.2f)", righty, rightx, lefty, leftx);
+        telemetry.addData("Speed", "Power: (%.2f)", power);
+        telemetry.addData("Y button", gamepad1.y);
+
+
+    }
+
+    /*@Override
+    public void stop() {
+    }*/
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+
+
+public class RobotHardware {
+
+    private HardwareMap hwMap = null;
+
+    public DcMotor leftFront = null;
+    public DcMotor rightFront = null;
+    public DcMotor leftBack = null;
+    public DcMotor rightBack = null;
+
+    public RobotHardware() {}
+
+    public void init(HardwareMap aHWMap) {
+
+        hwMap = aHWMap;
+
+        // Here we are obtaining the hardware mappings for the motors
+        leftFront = hwMap.get(DcMotor.class, "leftFront");
+        rightFront = hwMap.get(DcMotor.class, "rightFront");
+        leftBack = hwMap.get(DcMotor.class, "leftBack");
+        rightBack = hwMap.get(DcMotor.class, "rightBack");
+
+
+        // Hardware Mappings for Color & Distance Sensors
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        rightFront.setDirection(DcMotor.Direction.FORWARD);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setDirection(DcMotor.Direction.FORWARD);
+
+    }
+
+}
